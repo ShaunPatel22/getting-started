340d21a8f7cc8a18ec9faad41254d00f8358515a
ShaunPatel22 <108300148+ShaunPatel22@users.noreply.github.com>
2022-06-28 10:04:04 -0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+def add(a, b):
+    print(a+ b)
+    
+add(2, 5)
+add(100, 1)
+add(248, 2)
+    
+
+
+# In[18]:
+
+
+def remind(goal):
+    print('Today I have ' + goal)
+    
+remind('Piano lesson')
+remind ('Golf Game at 4')
+remind ('Class')
+
+
+# In[ ]:
+
+
+
+
